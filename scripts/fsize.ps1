057c6eafc25f5340d951fa0748a4f6322b92241e
skrasek.michael <skrasek.michal@email.cz>
2022-08-24 22:54:08 +0200

    fixed path check

--- scripts/fsize.ps1
+++ scripts/fsize.ps1
@@ -4,7 +4,9 @@ param (
 
 Import-Module Utils
 
-if (Test-Path $Path) {
-	$Measure = Get-ChildItem $Path -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum
+if (Test-Path $Path -PathType Container) {
+	$Measure = Get-ChildItem -Path $Path -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum
 	return Format-Size -Size $Measure.Sum
+} else {
+	Write-Error "Parameter [$Path] isn't path to directory."
 }
