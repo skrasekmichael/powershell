825aabddf3e02bf1f6b2465cb0e6530dfb5fbfd3
skrasek.michael <skrasek.michal@email.cz>
2024-09-07 14:25:28 +0200

    fix move-window

--- scripts/move-window.ps1
+++ scripts/move-window.ps1
@@ -4,6 +4,25 @@ param(
 	[int]$Delay = 5
 )
 
+if (-not [System.Management.Automation.PSTypeName]'User32'.Type) {
+    Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class User32 {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        public static readonly IntPtr HWND_TOP = new IntPtr(0);
+        public const uint SWP_NOSIZE = 0x0001;
+    }
+"@
+}
+
 Write-Output "Select window to move, window moves in $($Delay)"
 while ($Delay -gt 0) {
 	Start-sleep 1
